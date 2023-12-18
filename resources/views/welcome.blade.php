f9cf91169301d64f02341c8cc0f80e68666ba776
muhammmadalief19 <aliefmuhammad1908@gmail.com>
2023-12-19 00:25:06 +0700

    feat(message):menambah fitur send message landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -452,25 +452,26 @@
                     </div>
                 </div>
                 <div class="w-full h-auto">
-                    <form action="" method="post" class="w-full flex flex-col gap-5">
+                    <form action={{ route('message.store') }} method="post" class="w-full flex flex-col gap-5">
+                        @csrf
                         <div>
                             <label for="name"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                            <input type="text" id="name"
+                            <input type="text" id="name" name="name"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Your Name" required>
                         </div>
                         <div>
                             <label for="email"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                            <input type="text" id="email"
+                            <input type="text" id="email" name="email"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Your Email" required>
                         </div>
                         <div>
                             <label for="subject"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Subject</label>
-                            <input type="text" id="subject"
+                            <input type="text" id="subject" name="subject"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Subject" required>
                         </div>
@@ -478,11 +479,11 @@
                             <label for="message"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white capitalize">
                                 message</label>
-                            <textarea id="message" rows="4"
+                            <textarea id="message" rows="4" name="message"
                                 class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Message"></textarea>
                         </div>
-                        <button type="button" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 capitalize">
+                        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 capitalize">
                             send message
                         </button>
                     </form>
