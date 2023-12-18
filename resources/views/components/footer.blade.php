e73a1eaacb4a01539b2844e86acbeddf91aa7e0a
muhammmadalief19 <aliefmuhammad1908@gmail.com>
2023-12-18 23:25:46 +0700

    build:membuat components footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,14 @@
+
+
+<footer class="bg-white rounded-lg shadow dark:bg-gray-900 w-full">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="#home" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src={{ asset('images/logo-hmps.svg') }} class="h-10 aspect-square" alt="Flowbite Logo" />
+            </a>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400 capitalize">© 2023 himpunan mahasiswa PENS sumenep</span>
+    </div>
+</footer>
+
