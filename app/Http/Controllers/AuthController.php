d5151a237230fce776da79d640d93b9634f7a28e
muhammmadalief19 <aliefmuhammad1908@gmail.com>
2023-12-21 17:53:23 +0700

    feat(controller):membuat controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function loginView() {
+        return view("auth.login");
+    }
+}
